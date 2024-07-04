2a9c24068f6c914ca81418ce83d480172001758a
kaishalam <alamkaishg1511@gmail.com>
2024-07-05 04:31:36 +0530

    build: spring boot project and folder structure completed

---
+++ user-authentication-backed/src/main/java/com/nuwaish/user_authentication_backed/UserAuthenticationBackedApplication.java
@@ -0,0 +1,13 @@
+package com.nuwaish.user_authentication_backed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationBackedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationBackedApplication.class, args);
+	}
+
+}
---
+++ user-authentication-backed/src/test/java/com/nuwaish/user_authentication_backed/UserAuthenticationBackedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuwaish.user_authentication_backed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationBackedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
